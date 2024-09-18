b0cb6f58d7d9ec9a6bff8a5b6384911931683c37
ErShivnandan <ec1912@global.org.in>
2024-09-18 17:14:03 +0530

    update simulation functionality added

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-let form = document.getElementById("formId")
-let formDiv = document.getElementById("formDivId")
-let changeSim = document.getElementById("changeSimulation")
+let form = document.getElementById("formId");
+let formDiv = document.getElementById("formDivId");
+let changeSim = document.getElementById("changeSimulation");
+let container = document.getElementsByClassName("container")[0];
 
+// form view update & Form handling
 
-function updateSimulation(flag){
-    if(flag){
-        formDiv.classList.add("displayAppend")
-        changeSim.classList.add("displayRemove")
-    }
-    else{
-        formDiv.classList.add("displayRemove")
-        changeSim.classList.add("displayAppend")
-    }
+function updateSimulation(flag) {
+  if (flag) {
+    container.removeChild(changeSim);
+    container.appendChild(formDiv);
+    changeSim.classList.add("displayRemove");
+  } else {
+    container.removeChild(formDiv);
+    container.appendChild(changeSim);
+    changeSim.classList.remove("displayRemove");
+  }
 }
 
-function updateFormPresence(flag){
-
-    if(flag==true){
-        formDiv.classList.add("displayRemove")
-        changeSim.classList.add("displayAppend")
-    }
-    else{
-        formDiv.classList.add("displayAppend")
-        changeSim.classList.add("displayRemove")
-    }
-
+function updateFormPresence(flag) {
+  if (flag) {
+    container.removeChild(formDiv);
+    container.appendChild(changeSim);
+    changeSim.classList.remove("displayRemove");
+  } else {
+    container.removeChild(changeSim);
+    container.appendChild(formDiv);
+    changeSim.classList.add("displayRemove");
+  }
 }
-form.addEventListener('submit', function(event){
-    event.preventDefault();
 
-    const flor = document.getElementById("numberOfFloor").value;
-    const lift = document.getElementById("numberOfLifts").value;
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    console.log("floor: ", flor)
-    console.log("lift: ", lift)
-    updateFormPresence(true)
+  const flor = document.getElementById("numberOfFloor").value;
+  const lift = document.getElementById("numberOfLifts").value;
+
+  console.log("floor: ", flor);
+  console.log("lift: ", lift);
+  updateFormPresence(true);
 });
