9fe896889a0d5a631353cb60c102c11d71fe61ac
ErShivnandan <ec1912@global.org.in>
2024-09-19 13:52:18 +0530

    dynamic lift aread append functionality added

--- script.js
+++ script.js
@@ -10,14 +10,13 @@ function updateSimulation(flag) {
   if (flag) {
     container.removeChild(changeSim);
     container.appendChild(formDiv);
-    container.removeChild(simulationArea)
+    container.removeChild(simulationArea);
     changeSim.classList.add("displayRemove");
     simulationArea.classList.add("displayRemove");
-
   } else {
     container.removeChild(formDiv);
     container.appendChild(changeSim);
-    container.appendChild(simulationArea)
+    container.appendChild(simulationArea);
     changeSim.classList.remove("displayRemove");
     simulationArea.classList.remove("displayRemove");
   }
@@ -29,14 +28,13 @@ function updateFormPresence(flag) {
     container.appendChild(changeSim);
     changeSim.classList.remove("displayRemove");
     simulationArea.classList.remove("displayRemove");
-    container.appendChild(simulationArea)
+    container.appendChild(simulationArea);
   } else {
     container.removeChild(changeSim);
     container.appendChild(formDiv);
-    container.removeChild(simulationArea)
+    container.removeChild(simulationArea);
     changeSim.classList.add("displayRemove");
     simulationArea.classList.add("displayRemove");
-
   }
 }
 
@@ -46,14 +44,56 @@ form.addEventListener("submit", function (event) {
   const flor = document.getElementById("numberOfFloor").value;
   const lift = document.getElementById("numberOfLifts").value;
 
-  console.log("floor: ", flor);
-  console.log("lift: ", lift);
+  // console.log("floor: ", flor);
+  // console.log("lift: ", lift);
   updateFormPresence(true);
+  generateSimulation(flor, lift);
 });
 
+function generateSimulation(flor, lift) {
+  console.log("called", flor, lift);
 
-// Creating simulation area dynamically 
+  simulationArea.innerHTML = "";
+  flag = true;
+  for (let i = flor; i >= 1; i--) {
+    const simulationDiv = document.createElement("div");
+    simulationDiv.classList.add("simulation");
 
-let simArea = document.getElementsByClassName("simulationArea");
+    const simulationHeading = `
+      <div class="simulationHeading">
+          <h4>Floor : ${i}</h4>
+          <span>
+            <button class="button-3">Up</button>
+          </span>
+        </div>
+    `;
 
-console.log(simArea)
+    if (flag == true && i == 1) {
+      let liftContainers = "";
+
+      for (let j = lift; j >= 1; j--) {
+        liftContainers += `
+        <div class="lift-container">
+            <div class="door-left"></div>
+            <div class="door-right"></div>
+          </div>
+      `;
+      }
+
+      const liftArea = `
+        <div class="liftArea">
+        ${liftContainers}
+        </div>
+      `;
+      simulationDiv.innerHTML = simulationHeading + liftArea;
+      simulationArea.appendChild(simulationDiv);
+
+      flag = false;
+    } else {
+      simulationDiv.innerHTML = simulationHeading;
+      simulationArea.appendChild(simulationDiv);
+    }
+  }
+
+  simulationArea.classList.remove("displayRemove");
+}
